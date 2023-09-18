d7a5ca275b163c5447274f9c4b42a9cab4522c69
Pasith Pruksakorn <mickyza86@gmail.com>
2023-09-18 22:53:25 +0700

    Fifth try - for assignment2

---
+++ walletapp/app.py
@@ -0,0 +1,16 @@
+def hello():
+    print('Ad Astra Abyssosque!!')
+
+i = 1
+sum = 0
+while i < 6:
+  sum += i
+  i += 1
+print('sum is', sum)
+
+scores = [5, 8, 7, 3]
+max = 0
+for x in scores:
+  if x > max:
+    max = x
+print ('Maximum value is', max)
